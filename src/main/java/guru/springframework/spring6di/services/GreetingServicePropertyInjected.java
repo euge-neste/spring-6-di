90fc999ebb02315c211b2dc66eb225f6351239e6
euge-neste <yevhen.nesterko@gmail.com>
2025-06-19 12:25:18 +0300

    performing DI with Spring

---
+++ src/main/java/guru/springframework/spring6di/services/GreetingServicePropertyInjected.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6di.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("propertyGreetingService")
+public class GreetingServicePropertyInjected implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Friend dont let friends do property injection";
+    }
+}
